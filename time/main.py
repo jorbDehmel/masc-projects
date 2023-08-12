483479cdc57a02f083f509b0a0d746545495b800
Jorb Dehmel <jdehmel@outlook.com>
2023-08-12 13:26:12 -0600

    Misc

--- time/main.py
+++ time/main.py
@@ -1,14 +1,44 @@
 #!/usr/bin/python3
 # The above line allows this file to be executable (shebang)
-# This is designed to run on a Raspberry Pi with only a mouse
+
+# Jordan Dehmel, 2023
+# jdehmel@outlook.com
+# jedehmel@mavs.coloradomesa.edu
+
+'''
+This is a short exhibit for a kiosk in the
+Grand Junction Math and Science Center (Eureka!).
+It demonstrates how computers tell time, talks
+about the 2038 integer overflow issue, what
+integer overflow is, and how computer scientists
+have solved it.
+
+All images used are stored in the images directory.
+You should be able to launch this program by
+double-clicking main.py from within the time
+directory (because of the shebang), but if not
+you can run the command
+`python3 /path/to/file/here/main.py` to start it.
+
+This program is designed to function with only
+a mouse, but a keyboard is required to exit it.
+To close the program, simply press escape.
+After escape is pressed, the Raspberry Pi running
+it can be powered down safely and unplugged.
+
+This program and all associated source code files
+are FOSS under the GPLv3, a copy of which should
+be attached here. Designed for public educational
+use.
+'''
 
 from time_driver import *
 
-
 if __name__ == '__main__':
-    window = TimeApplication()
-    window.first_screen()
-    window.root.mainloop()
-
-    sys.exit(0)
+    try:
+        window = TimeApplication()
+        window.first_screen()
+        window.root.mainloop()
+    except:
+        print("Unknown failure occured. Restarting program...")
 
--- time/time_driver.py
+++ time/time_driver.py
@@ -10,9 +10,13 @@ from tkinter import ttk
 from ttkthemes import ThemedTk
 
 import time
-import sys
 
+# Get the trimmed & padded & byte-spaced binary version of a number, limited
+# to a certain number of bits (digits)
 def get_bin(what: int, digits: int) -> str:
+    if what + pow(2, digits - 1) >= pow(2, digits):
+        return ("11111111 " * (digits // 8))[:-1]
+
     # Get raw binary (without 0b at the beginning)
     out: str = bin(what + pow(2, digits - 1))[2:]
 
@@ -29,6 +33,13 @@ def get_bin(what: int, digits: int) -> str:
 
     return out
 
+# A number for the second screen; This is the starting point
+# to demonstrate the integer overflow of 2038
+overflow_constant: int = pow(2, 31) - 10
+
+# A safer version of ctime: If it is within the range
+# representable, it will return that. Otherwise, it will
+# return a rough estimate of the year (CE / BCE)
 def safe_ctime(what: int) -> str:
     out: str = ""
     
@@ -46,13 +57,25 @@ def safe_ctime(what: int) -> str:
 
     return out
 
+# The actual application running the computer time exhibit
 class TimeApplication:
+    # Initialize all needed member variables
     def __init__(self) -> None:
         # Create members, but do not start window yet
         self.root: ThemedTk = ThemedTk(theme="breeze")
         
         # Activate fullscreen mode
         self.root.attributes('-fullscreen', True)
+
+        try:
+            # Add background image via label
+            self.photo = tk.PhotoImage(file='images/bg.png')
+            self.bg_image: ttk.Label = ttk.Label(self.root, image=self.photo)
+            self.bg_image.place(x=0, y=0, relheight=1.0, relwidth=1.0)
+        except:
+            print("Failed to open background image")
+            self.photo = None
+            self.bg_image = None
 
         # Add frame
         self.frame: ttk.Frame = ttk.Frame(self.root)
@@ -87,6 +110,8 @@ class TimeApplication:
 
         return
     
+    # Close the application. Minimizes, then destroys, then exits
+    # in order to have three backup systems.
     def close(self, event) -> None:
         # Just in case
         self.root.attributes('-fullscreen', False)
@@ -94,63 +119,102 @@ class TimeApplication:
         # Should work
         self.root.destroy()
 
-        # Just in case it doesn't work
-        sys.exit(0)
-
         return
     
+    # Janky way to update the slider and its variable
+    # every time it is moved
     def on_slider_change(self, event) -> None:
-        self.time_mode = "slider"
+        if self.outgoing != "":
+            self.root.after_cancel(self.outgoing)
+    
         self.slider_var = self.slider.get()
 
-        if self.current_screen == "first":
-            self.first_screen()
-        elif self.current_screen == "third":
-            self.third_screen()
+        if self.time_mode != "NULL":
+            self.time_mode = "slider"
+
+            # Update screen
+            if self.current_screen == "first":
+                self.first_screen()
+            elif self.current_screen == "third":
+                self.third_screen()
 
         return
     
+    # The function called when the 'min' button is pressed
+    # Sets the slider and its variable tot eh minimum position.
+    # On screen one, the lowest 32-bit integer. On screen three,
+    # the lowest 64-bit integer
     def min(self) -> None:
-        self.time_mode = "slider"
+        self.time_mode = "NULL"
 
         if self.current_screen == "first":
             self.slider_var = -pow(2, 31)
+            self.slider.set(self.slider_var)
+            self.time_mode = "slider"
+            
             self.first_screen()
+
         elif self.current_screen == "third":
             self.slider_var = -pow(2, 63)
+            self.slider.set(self.slider_var)
+            self.time_mode = "slider"
+            
             self.third_screen()
     
         return
     
+    # Same as min, but for the maximal value of the respective
+    # integer representations.
     def max(self) -> None:
-        self.time_mode = "slider"
+        self.time_mode = "NULL"
 
         if self.current_screen == "first":
             self.slider_var = pow(2, 31) - 1
+            self.slider.set(self.slider_var)
+            self.time_mode = "slider"
+
             self.first_screen()
+
         elif self.current_screen == "third":
             self.slider_var = pow(2, 63) - 1
+            self.slider.set(self.slider_var)
+            self.time_mode = "slider"
+
             self.third_screen()
     
         pass
 
+    # Zeroes out the slider and its variable, no matter the screen
     def zero(self) -> None:
-        self.time_mode = "slider"
+        self.time_mode = "NULL"
         self.slider_var = 0.0
+        self.slider.set(0)
 
         if self.current_screen == "first":
+            self.time_mode = "slider"
             self.first_screen()
+
         elif self.current_screen == "third":
+            self.time_mode = "slider"
             self.third_screen()
 
         return
 
+    # Sets the slider to the current time's position, no matter
+    # the screen. Does not regularly update because you can't really
+    # notice the change over a short period of time.
     def now(self) -> None:
-        self.time_mode = "now"
+        self.time_mode = "NULL"
+        self.slider.set(time.time())
 
         if self.current_screen == "first":
+            self.time_mode = "now"
+
             self.first_screen()
+
         elif self.current_screen == "third":
+            self.time_mode = "now"
+
             self.third_screen()
         
         return
@@ -198,7 +262,7 @@ class TimeApplication:
             self.c_time_label.pack()
 
             ttk.Label(self.frame,
-                text="\nMove the slider to see time like a computer!\n",
+                text="\nMove the slider to change the time!\n",
                 font=("Arial", 12)).pack()
 
             # Slider for interactivity
@@ -209,7 +273,8 @@ class TimeApplication:
                 orient='horizontal',
                 variable=self.slider_var,
                 command=self.on_slider_change,
-                length=500
+                length=1000,
+                value=time.time()
             )
             self.slider.pack()
 
@@ -228,7 +293,7 @@ class TimeApplication:
             # Max button
             ttk.Button(button_holder, text="Max", command=self.max).grid(column=4, row=0)
 
-            ttk.Label(self.frame, text="What happens after max?", font=("Arial", 12)).pack()
+            ttk.Label(self.frame, text="\nWhat happens after max?", font=("Arial", 12)).pack()
             ttk.Button(self.frame, text="Click here to find out", command=self.second_screen).pack()
 
             ttk.Label(self.frame, text="\nWhat has improved?", font=("Arial", 12)).pack()
@@ -257,15 +322,70 @@ class TimeApplication:
 
         return
     
+    # Secondary screen; Explains what integer overflow is
+    # and demonstrates it occuring in 2038.
     def second_screen(self) -> None:
         if self.current_screen != "second":
             if self.outgoing != "":
                 self.root.after_cancel(self.outgoing)
+            
+            self.clear()
+
+            ttk.Label(self.frame, text="The 2038 Problem", font=("Arial", 25)).pack()
+            ttk.Label(self.frame, text="How could 99 + 1 = 0?", font=("Arial", 16)).pack()
+            ttk.Label(self.frame, text="What is 99 + 1? 100! But what if we only have two digits to write the answer?\n", font=("Arial", 12)).pack()
+            ttk.Label(self.frame, text="We would have to write 99 + 1 = 00!\n", font=("Arial", 12)).pack()
+            ttk.Label(self.frame, text="This same thing happens to computers! Here's what a computer sees:", font=("Arial", 12)).pack()
+
+            ttk.Label(self.frame, text="11111111 + 1 = ?", font=("Monospace", 12)).pack()
+            ttk.Label(self.frame, text="11111111 + 1 = 1 00000000", font=("Monospace", 12)).pack()
+
+            ttk.Label(self.frame, text="\nJust like 99 + 1, the computer needs an extra digit for the answer.", font=("Arial", 12)).pack()
+            ttk.Label(self.frame, text="\nThis means that a computer might see this as zero!", font=("Arial", 12)).pack()
+            ttk.Label(self.frame, text="\nThis is called overflow, and it can be a problem for computer time!\n", font=("Arial", 12)).pack()
+
+            # Binary label
+            self.bin_label = ttk.Label(self.frame, font=("Monospace", 12))
+            self.bin_label.pack()
+
+            # Raw UNIX timecode label
+            self.raw_time_label = ttk.Label(self.frame,
+                                        font=("Monospace", 12))
+            self.raw_time_label.pack()
+
+            # ctime label (human readable)
+            self.c_time_label = ttk.Label(self.frame,
+                                         font=("Monospace", 12))
+            self.c_time_label.pack()
+
+            ttk.Label(self.frame, text="\nThis will make some computers think 2038 is 1901.", font=("Arial", 12)).pack()
+
+            ttk.Label(self.frame, text="\n\nBut don't worry! We have a solution!", font=("Arial", 12)).pack()
+            ttk.Button(self.frame, text="Click here to find out", command=self.third_screen).pack()
+
+            ttk.Label(self.frame, text="\nOr you can go back to our old clock.", font=("Arial", 12)).pack()
+            ttk.Button(self.frame, text="Click here to go back", command=self.first_screen).pack()
 
             self.current_screen = "second"
 
-        pass
+        self.cur_time = int(time.time() % 20) + overflow_constant
+        if self.cur_time >= pow(2, 31):
+            self.cur_time -= pow(2, 32)
 
+        # Get padded binary representation
+        real_bin: str = get_bin(self.cur_time, 32)
+
+        # Construct this screen
+        self.bin_label.config(text=real_bin)
+        self.raw_time_label.config(text=str(self.cur_time))
+        self.c_time_label.config(text=safe_ctime(self.cur_time))
+
+        # After 1 second, call this function again
+        self.outgoing = self.root.after(1000, self.second_screen)
+
+        return
+
+    # Demonstrates the "new" 64-bit integer representation of computer time
     def third_screen(self) -> None:
         if self.current_screen != "third":
             if self.outgoing != "":
@@ -277,7 +397,7 @@ class TimeApplication:
             # Title, subtitle, subsubtitle
             ttk.Label(self.frame, text="What's New in Computer Time?", font=("Arial", 25)).pack()
             ttk.Label(self.frame,
-                text="Nowadays, we use twice as many bytes to store the time!\n",
+                text="Nowadays, we use twice as much space to store the time!\n",
                 font=("Arial", 16)).pack()
             ttk.Label(self.frame,
                 text="Here's what a computer sees now:",
@@ -297,8 +417,10 @@ class TimeApplication:
             self.raw_time_label.pack()
 
             ttk.Label(self.frame,
-                text="\nThis can store 4,294,967,296 times as many times!\n" +
-                    "This number is the date:",
+                text="\nThis can store 4,294,967,296 times as much!\n",
+                font=("Arial", 12)).pack()
+            ttk.Label(self.frame,
+                text="This number is about:",
                 font=("Arial", 12)).pack()
 
             # ctime label (human readable)
@@ -307,7 +429,7 @@ class TimeApplication:
             self.c_time_label.pack()
 
             ttk.Label(self.frame,
-                text="\nMove the slider to see how many dates you can see!\n",
+                text="\nMove the slider to change the time!\n",
                 font=("Arial", 12)).pack()
 
             # Slider for interactivity
@@ -318,9 +440,11 @@ class TimeApplication:
                 orient='horizontal',
                 variable=self.slider_var,
                 command=self.on_slider_change,
-                length=500
+                length=1000
             )
             self.slider.pack()
+
+            ttk.Label(self.frame, text="\nNow overflow won't happen for another 292 billion years!", font=("Arial", 12)).pack()
 
             button_holder: ttk.Frame = ttk.Frame(self.frame)
             button_holder.pack(pady=50)
@@ -337,10 +461,10 @@ class TimeApplication:
             # Max button
             ttk.Button(button_holder, text="Max", command=self.max).grid(column=4, row=0)
 
-            ttk.Label(self.frame, text="What came first?", font=("Arial", 12)).pack()
+            ttk.Label(self.frame, text="\n\nWhat came first?", font=("Arial", 12)).pack()
             ttk.Button(self.frame, text="Click here to find out", command=self.first_screen).pack()
 
-            ttk.Label(self.frame, text="\Why do we need this?", font=("Arial", 12)).pack()
+            ttk.Label(self.frame, text="\nWhat's overflow?", font=("Arial", 12)).pack()
             ttk.Button(self.frame, text="Click here to find out", command=self.second_screen).pack()
 
             self.current_screen = "third"
@@ -361,6 +485,7 @@ class TimeApplication:
         self.c_time_label.config(text=safe_ctime(self.cur_time))
 
         # After 1 second, call this function again
-        self.root.after(1000, self.third_screen)
+        self.outgoing = self.root.after(1000, self.third_screen)
 
         return
+
