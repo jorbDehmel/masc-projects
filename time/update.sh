00545f57498a44b7340347efab359ab117adccbb
Jorb Dehmel <jdehmel@outlook.com>
2023-08-16 13:47:12 -0600

    Improved update script

--- time/update.sh
+++ time/update.sh
@@ -5,9 +5,10 @@
 # Default raspi username: pi password: raspberry
 
 echo "This script updates the time exhibit when run."
+echo "jdehmel@outlook.com, 2023"
 
 # Check for wifi
-if ! ping google.com -c 1 > /dev/null ; then echo "ERROR: No internet connection!" ; exit 1 ; fi
+if ! ping google.com -c 1 > /dev/null ; then echo "ERROR: No internet connection!" ; fi
 
 echo "Checking for project updates..."
 git pull > log.txt
@@ -21,5 +22,18 @@ sudo apt-get install -y python3 python-tk python3-pil.imagetk
 
 echo "Installing Python packages (this may take a while)..."
 sudo pip install ttkthemes pillow
+
+echo "Making app start on system powerup..."
+
+# Copy old crontab
+touch newcron
+if crontab -l ; then crontab -l > newcron ; fi
+
+# Append new command to it
+echo "@reboot python3 "$(pwd)"/main.py" >> newcron
+
+# Put new crontab into place
+crontab newcron
+rm newcron
 
 echo "Done."
