273ccfec5d7eae96e508a9833b63a3ce8be8e7e9
Jorb Dehmel <jdehmel@outlook.com>
2023-08-16 11:42:44 -0600

    Improved update script

--- time/update.sh
+++ time/update.sh
@@ -2,6 +2,7 @@
 # Jordan Dehmel, 2023
 # jdehmel@outlook.com, jedehmel@mavs.coloradomesa.edu
 # github.com/jorbDehmel/masc-projects
+# Default raspi username: pi password: raspberry
 
 echo "This script updates the time exhibit when run."
 
@@ -11,5 +12,9 @@ git pull > /dev/null
 echo "Fixing permissions..."
 chmod +x ./main.py > /dev/null
 chmod +x ./update.sh > /dev/null
+
+echo "Ensuring Python is up to date..."
+sudo apt-get install python3 python-tk python3-pil.imagetk > /dev/null
+sudo pip install tkinter ttkthemes pillow
 
 echo "Done."
